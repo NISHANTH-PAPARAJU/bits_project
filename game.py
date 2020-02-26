8d8d9bfee1acb40c61f7f35bf684c75613edee69
siriusblack189 <siva2thrones@gmail.com>
2020-02-27 00:44:57 +0530

    added back ground color

--- game.py
+++ game.py
@@ -2,7 +2,9 @@ import pygame
 
 
 class Game:
-    screen = None
+
+    BACK_GROUND_COLOR = 0,0,0
+
     done = False
     screen = None
 
@@ -10,10 +12,12 @@ class Game:
     def __init__(self, S_H, S_W):
         pygame.init()
         self.screen = pygame.display.set_mode( (S_W, S_H)) 
+        self.screen.fill(self.BACK_GROUND_COLOR)
+        #pygame.display.update()
         self.done = False
 
     def loadImage(self):
-        image = pygame.image.load(r'.\data\roundedBlock.png') 
+        image = pygame.image.load(r'./data/roundedBlock.png') 
         self.screen.blit(image, (0, 0))
 
     def displayGame(self):
@@ -21,7 +25,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
+
             self.loadImage()
+            #self.screen.fill(self.BACK_GROUND_COLOR)
             pygame.display.flip()
 
 

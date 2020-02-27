c7b004ede511b852a4599950f8277773737d1b04
siriusblack189 <siva2thrones@gmail.com>
2020-02-28 02:28:21 +0530

    added shape to the game

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ class Game:
 
     BACK_GROUND_COLOR = 0,0,0
     GREEN = (0, 200, 0 )
+    DEFAULT_POS = 13
 
     done = False
     screen = None
@@ -20,7 +21,7 @@ class Game:
     j_shape = None
     L_shape = None
   
-    container = None
+    container = []
 
     t_shape_a = [ [1, 1, 1],
                   [0, 1, 0]
@@ -69,27 +70,34 @@ class Game:
         self.b_width = self.block.get_size()[0]
         self.b_height = self.block.get_size()[1]
         
-        self.cols = self.s_w / self.b_width
-        self.rows = self.s_h / self.b_height
-        print (self.cols)
-        print (self.rows)
-        self.container = [[0]*self.cols]*self.rows
+        self.cols = self.s_w // self.b_width
+        self.rows = self.s_h // self.b_height
+#        self.container = [[0]*self.cols]*self.rows
+        self.make_2darray()
+
+    def make_2darray(self):
+        for j in range(self.rows):
+            column = []
+            for i in range(self.cols):
+                 column.append(0)
+            self.container.append(column)
 
     # draw method to draw the shapes of tetris symbols
     def drawShape(self, arr, x, y):
         rows = len(arr)
         cols = len(arr[0])
-
+        print (rows)
         x = self.b_height * x
         y = self.b_width * y
-
-        for i in range(cols):
-            for j in range(rows):
-                try:
-                    if arr[j][i] == 1:
-                        self.screen.blit(self.block, ((x+ ( self.b_height *i )), (y+ (self.b_width *j))))
-                except:
-                    print ('error ' + str(i))
+        j = 0
+        for row in self.container:
+            for i in range(len(row)):
+                if arr[j][i] == 1:
+                    self.screen.blit(self.block, ((x+ ( self.b_height *i )), (y+ (self.b_width *j))))
+            j = j + 1
+            print ()      
+   
+   
                     
     # load the basic block of tetris
     def loadImage(self):
@@ -102,14 +110,28 @@ class Game:
     def drawLine(self, x, y, e_x, e_y):
         pygame.draw.line(self.screen, self.GREEN, [x, y], [e_x, e_y], 2)  
 
-    def addSymbolToGame(self):
-        pass
-    
+    def addSymbolToGame(self, arr):
+         row = len(arr)
+         col = len(arr[0])
+         print (row)
+         for i in range(col):
+             for j in range(row):
+                 self.container[ j][i+self.DEFAULT_POS]= arr[j][i]  
+
+    def printContainer(self):
+        for row in self.container:
+            print (row) 
+       
     # main game loop
     def displayGame(self):
         #self.drawShape(self.t_shape_a, 0, 0) 
         #self.drawShape(self.l_shape_a, 5, 0) 
         #self.drawShape(self.L_shape_a, 8, 0) 
+        #self.drawShape(self.container, 0, 0)
+        #self.printContainer()
+        self.addSymbolToGame(self.t_shape_a)
+#        self.container[1][1] = 1
+        self.printContainer()
         self.drawShape(self.container, 0, 0)
         while not self.done:
             for event in pygame.event.get():

27908c746c82fd57e11eeb9d7d43c554f6923b6a
siriusblack189 <siva2thrones@gmail.com>
2020-05-03 21:57:47 +0530

    added new train method

--- train.py
+++ train.py
@@ -1,67 +1,72 @@
-import torch
-import torch.nn as nn
+import tensorflow.keras as tf
+import tensorflow as kf
 import numpy as np
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from torch import optim
 
-class dataset(Dataset):
-    def __init__(self, x, y):
-        self.x = x 
-        self.y = y
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
-
-class Net(nn.Module):
-
+class Model:
     def __init__(self):
-       super(Net, self).__init__()
-       self.a = nn.Linear(16*1, 1)
+        self. model = tf.Sequential()
+        self.model.add(tf.layers.Conv2D(
+                input_shape=[1, 20,20],
+                filters=32,
+                kernel_size=5,
+                strides=5,
+                activation=kf.nn.tanh,
+                padding='same',
+                kernel_initializer=kf.keras.initializers.VarianceScaling()
+                ))
+        
+        self.model.add(tf.layers.Conv2D(
+                filters=64,
+                kernel_size=3,
+                strides=2,
+                activation=kf.nn.tanh,
+                padding='same',
+                kernel_initializer=kf.keras.initializers.VarianceScaling()
+                ))
+        self.model.add(tf.layers.Conv2D(
+                filters=128,
+                kernel_size=3,
+                strides=1,
+                activation=kf.nn.tanh,
+                padding='same',
+                kernel_initializer=kf.keras.initializers.VarianceScaling()
+                ))
+        self.model.add(tf.layers.Flatten())
+        self.model.add(tf.layers.Dense(
+                 units=512,
+                 activation=kf.nn.tanh,
+                 kernel_initializer=kf.keras.initializers.VarianceScaling()
+                 ))
+        self.model.add(tf.layers.Dense(
+                units=256,
+                activation=kf.nn.tanh,
+                kernel_initializer=kf.keras.initializers.VarianceScaling()
+                ))
+        self.model.add(tf.layers.Dense(
+                units=44,
+                kernel_initializer=kf.keras.initializers.VarianceScaling(),
+                activation=kf.nn.softmax
+                ))
+        self.model.compile(optimizer=tf.optimizers.Adam(0.0005),loss=tf.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+        self.model.summary()
 
-    def forward(self, x):
-        x = F.relu(self.a(x))
-        return torch.sigmoid(x)
+    def trainModel(self, x, y):
+        h = self.model.fit(states, actions, batch_size=1, epochs=1, validation_data=[states, actions])
 
-if __name__ == "__main__": 
-    data = []
-    target = []
-    for i in range(1, 2**8):
-        result = 0
-        if i%2 == 0:
-          result  = 1 
-        res = [ int(i) for i in ( '{:016b}'.format(i))]
-        data.append(np.array(res))
-        target.append(result) 
-    x = data
-    y = target
-    data_set = dataset(x, y)
-    train_data = torch.utils.data.DataLoader(data_set, batch_size = 10, shuffle=True)
-    model = Net()
-    optimizer = optim.Adam(model.parameters()) 
-    device = 'cpu'
-    floss = nn.MSELoss()
-    model.train()
-    for i in range(100):
-        all_loss = 0
-        num_loss = 0
-        for batch_idx, (data, target) in enumerate(train_data):
-            target = target.unsqueeze(-1)
-            data, target = data.to(device), target.to(device)
-            target = target.float()
-            data = data.float()
-    
-            optimizer.zero_grad()
-            output = model(data)
-    
-            loss = floss(output, target)
-            loss.backward()
-            optimizer.step()
-    
-            all_loss += loss.item()
-            num_loss += 1
-        print ('%3d: %f' %(i, all_loss/num_loss))
-    torch.save(model.state_dict(), 'model.pth')
+    def saveModel(self):
+        self.model.save('model_save.h5')
+
+    def loadModel(self, f):
+        self.model = tf.load_model('model_save.h5')
+
+    def predict(self, state):
+        output = self.model.predict(state.reshape([1,1,20,20]))
+        return np.argmax(output[0])
+        
+if __name__ == "__main__":
+    m = Model()
+    with np.load('./all_game/all_game.npz') as data:
+         states  = data['arr_0']
+         actions = data['arr_1']
+    m.trainModel(states, actions)
+    m.saveModel()
